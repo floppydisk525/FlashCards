0c66308b7537ad9a4a12e3717c34b0ab05c1af66
floppydisk525 <mark@ijohnsen.com>
2022-07-23 14:53:32 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Configuration;
+
+namespace FlashCards
+{
+    class Program
+    {
+        //static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
